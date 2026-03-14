90e7b5f4c27ea6234d856f240ec4c0a017a24ec6
Dr1x-clock <komlakovdima6@gmail.com>
2026-03-14 10:01:05 +0300

    SADQWQEQWQEREERWER

--- TeamWork/Program.cs
+++ TeamWork/Program.cs
@@ -10,7 +10,7 @@ namespace TeamWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello WSDSDWorld!");
         }
     }
 }
